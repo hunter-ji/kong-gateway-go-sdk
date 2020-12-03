e012237da7c02a1ecd443fcf05bb65c77e0ded43
Kuari <kuari@justmylife.cc>
2020-12-03 13:33:17 +0800

    feat:init project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kuari/kong-gateway-go-sdk
+
+go 1.14
---
+++ hello.go
@@ -0,0 +1,13 @@
+package kong_gateway_go_sdk
+
+import "fmt"
+
+// Hello says hello.
+func Hello() {
+	fmt.Println("Hello go mod!")
+}
+
+// Bye says bye.
+func Bye() {
+	fmt.Println("Bye go mod!")
+}
